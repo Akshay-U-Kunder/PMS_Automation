c728ef82ecdf9f28d6cd3186192ba5dfa264bfa6
Akshay-U-Kunder <akshayputtur6@gmail.com>
2023-12-18 15:37:11 +0530

    TM locators converted to xpath1

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+/// <reference types="Cypress" />
+/// <reference types="Cypress-xpath" />
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -24,8 +26,7 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-/// <reference types="Cypress" />
-/// <reference types="Cypress-xpath" />
+
 
 import 'cypress-file-upload';
 //require('cypress-downloadfile/lib/downloadFileCommand')
